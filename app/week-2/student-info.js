8e513b0519562d355e05bc1d7bebc327a0f8f487
AhmadAl79 <165332909+AhmadAl79@users.noreply.github.com>
2025-09-25 10:42:38 -0600

    feat(week-2): add StudentInfo component and week-2 page; update root page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Name: Ahmad Al Khodr</p>
+      <p>
+        GitHub:{" "}
+        <Link href="https://github.com/AhmadAL79/cprg306-assignments">
+          AhmadAL79/cprg306-assignments
+        </Link>
+      </p>
+    </div>
+  );
+}
