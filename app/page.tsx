33c794ae3691aeb14cc70e9c639375a2af93fe08
AhmadAl79 <165332909+AhmadAl79@users.noreply.github.com>
2025-10-31 10:42:05 -0600

    Updates

--- app/page.tsx
+++ app/page.tsx
@@ -52,10 +52,18 @@ export default function Home() {
         </Link>
       </p>
 
+      <p>
+        <Link
+          href="/week-7"
+          className="text-blue-600 underline hover:text-blue-800"
+        >
+          Go to Week 7
+        </Link>
+      </p>
+
       <p className="text-gray-600">
         Additional info: This is my assignment project setup.
       </p>
     </main>
   );
 }
-
